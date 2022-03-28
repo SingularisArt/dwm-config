3dee10e3ce95978253528d7ae76d3a82eed3d837
Hashem A. Damrah <dermdorm2@gmail.com>
2022-03-27 21:00:52 -0700

    Update config.h by moving keys to a seperate file

--- config.h
+++ config.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include "tcl.c"
 
 /* Constants */
 #define TERMINAL "xfce4-terminal"
@@ -8,25 +9,25 @@
 #define AltMask Mod1Mask
 #define PDF_VIEWER "zathura"
 #define CURRENTCOURSE "~/Documents/notes/current-course"
+#define MAIN_PATH "~/Singularis/local/school/main.py"
 
-/* appearance */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const double defaultopacity  = 0.75;
-
-static unsigned int borderpx        = 3;
-static unsigned int snap            = 32;
-static unsigned int gappih          = 20;
-static unsigned int gappiv          = 10;
-static unsigned int gappoh          = 10;
-static unsigned int gappov          = 30;
-static int swallowfloating          = 0;
-static int smartgaps                = 0;
+// Appearance
+static const unsigned int borderpx  = 3;    // border pixel of windows
+static const unsigned int snap      = 25;   // snap pixel
+static const unsigned int gappih    = 20;   // horiz inner gap between windows
+static const unsigned int gappiv    = 10;   // vert inner gap between windows
+static const unsigned int gappoh    = 10;   // horiz outer gap between windows and screen edge
+static const unsigned int gappov    = 30;   // vert outer gap between windows and screen edge
+static const int swallowfloating    = 0;    // 1 means swallow floating windows by default
+static const int smartgaps          = 0;    // 1 means no outer gap when there is only one window
+static const int showbar            = 1;    // 0 means no bar
+static const int topbar             = 1;    // 0 means bottom bar
+static const int user_bh			      = 36;		// 0 means normal bar height, >1 means user bar height
 
 //static const char *fonts[]          = { "Noto Color Emoji:size=10",
-static const char *fonts[]          = { "monospace:size=9",
-                                        "fontawesome:size=9",
-                                        "JoyPixels:pixelsize=9" };
+static const char *fonts[]          = { "monospace:size=8",
+                                        "fontawesome:size=8",
+                                        "JoyPixels:pixelsize=8" };
 static const char dmenufont[]			  = "monospace:size=12";
 
 #include "/home/hashem/.cache/wal/colors-wal-dwm.h"
@@ -46,28 +47,26 @@ static Sp scratchpads[] = {
 /* tagging */
 static const char *tags[] = { "T", "W", "F", "B", "P", "A", "E", "S", "C" };
 
-static const Rule rules[] = {
-	// class   instance   title    	tags mask    isfloating   isterminal  noswallow  monitor
-	{ "Gimp",  NULL,      NULL,     1,           0,           0,          0,         -1 },
-	{ "Gimp",  NULL,      NULL,     1 << 8,      0,           0,          0,         -1 },
-};
-
 /* layout(s) */
 static float mfact     = 0.55;
 static int nmaster     = 1;
 static int resizehints = 0;
 #define FORCE_VSPLIT 1
+
 #include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "MIDDLE",	centeredmaster },
-
-	{ "Fibonacci",	spiral },
-	{ "DWINDLE",	dwindle },
-
-	{ "NULL",	NULL },
-	{ NULL,		NULL },
-
+ 	{ "[]=",	      tile },			              // Default: Master on left, slaves on right
+	{ "TTT",	      bstack },		              // Master on top, slaves on bottom
+	{ "H[]",	      deck },			              // Master on left, slaves in monocle-like mode on right
+	{ "[@]",	      spiral },		              // Fibonacci spiral
+	{ "[\\]",	      dwindle },		            // Decreasing in size right and leftward
+	{ "|M|",	      centeredmaster },		      // Master in middle, slaves on sides
+	{ ">M>",	      centeredfloatingmaster },	// Same but master floats
+	{ "|||",        tcl },                    // Column Layout
+  { "><>",	      NULL },			              // no layout function means floating behavior
+	{ NULL,		      NULL },
 };
 
 /* key definitions */
@@ -87,11 +86,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *termcmd[]  = { TERMINAL, NULL };
-static const char *filemanager[]  = { FILEBROWSER, NULL };
 
-/*
- * Xresources preferences to load at startup
- */
+// Xresources preferences to load at startup
 ResourcePref resources[] = {
 	{ "color0",						STRING,		&norm_border },
 	{ "color8",						STRING,		&sel_border },
@@ -117,115 +113,7 @@ ResourcePref resources[] = {
 #include <X11/XF86keysym.h>
 #include "shiftview.c"
 
-static Key keys[] = {
-  STACKKEYS(MODKEY,                          focus)
-  STACKKEYS(MODKEY|ShiftMask,                push)
-  TAGKEYS(			XK_1,		0)
-  TAGKEYS(			XK_2,		1)
-  TAGKEYS(			XK_3,		2)
-  TAGKEYS(			XK_4,		3)
-  TAGKEYS(			XK_5,		4)
-  TAGKEYS(			XK_6,		5)
-  TAGKEYS(			XK_7,		6)
-  TAGKEYS(			XK_8,		7)
-  TAGKEYS(			XK_9,		8)
-  { MODKEY,							                XK_w,							spawn,					SHCMD(BROWSER) },
-  { MODKEY,							                XK_F1,						spawn,					SHCMD("zathura ~/Singularis/master.pdf") },
-  { MODKEY,							                XK_BackSpace,			spawn,					SHCMD("$HOME/.config/rofi/applets/menu/powermenu.sh") },
-  { MODKEY,							                XK_d,							spawn,					SHCMD("rofi -show drun -show-icons") },
-  { MODKEY|ShiftMask,		                XK_e,							spawn,					SHCMD("rofi -show emoji -modi emoji") },
-  { MODKEY|ShiftMask,		                XK_a,							spawn,					SHCMD("$HOME/.config/rofi/applets/applets/apps.sh") },
-  { MODKEY,							                XK_0,							view,						{.ui = ~0 } },
-  { MODKEY|ShiftMask,		                XK_0,							tag,						{.ui = ~0 } },
-
-  { MODKEY,							                XK_Tab,						view,						{0} },
-  { MODKEY,							                XK_q,							killclient,			{0} },
-  { MODKEY,							                XK_t,							setlayout,			{.v = &layouts[0]} },
-  { MODKEY,							                XK_y,							setlayout,			{.v = &layouts[1]} },
-  { MODKEY,							                XK_u,							setlayout,			{.v = &layouts[2]} },
-  { MODKEY,							                XK_i,							setlayout,			{.v = &layouts[3]} },
-  { MODKEY,							                XK_T,							setlayout,			{.v = &layouts[4]} },
-  { MODKEY,							                XK_o,							incnmaster,			{.i = +1 } },
-  { MODKEY,							                XK_backslash,			view,						{0} },
-  { MODKEY|ShiftMask,		                XK_o,							incnmaster,			{.i = -1 } },
-  { MODKEY|ShiftMask,                   XK_q,							quit,						{0} },
-  { MODKEY,							                XK_a,							togglegaps,			{0} },
-  { MODKEY,							                XK_s,							togglesticky,		{0} },
-  { MODKEY,							                XK_f,							togglefullscr,	{0} },
-  { MODKEY,							                XK_g,							shiftview,			{ .i = -1 } },
-  { MODKEY,							                XK_h,							setmfact,				{.f = -0.05} },
-  { MODKEY,							                XK_l,							setmfact,      	{.f = +0.05} },
-  { MODKEY,							                XK_semicolon,			shiftview,			{ .i = 1 } },
-  { MODKEY,							                XK_apostrophe,		togglescratch,	{.ui = 1} },
-  { MODKEY,							                XK_Return,				spawn,					{.v = termcmd } },
-  { MODKEY|ShiftMask,		                XK_g,							shifttag,				{ .i = -1 } },
-  { MODKEY|ShiftMask,		                XK_semicolon,			shifttag,				{ .i = 1 } },
-  { MODKEY|ShiftMask,		                XK_Return,				togglescratch,	{.ui = 0} },
-
-  { MODKEY|ShiftMask,		                XK_a,				      spawn,	        SHCMD("$HOME/Singularis/local/scripts/anime") },
-  { MODKEY|ShiftMask,		                XK_n,				      spawn,	        SHCMD("$HOME/Singularis/local/scripts/netflix") },
-  { MODKEY|ShiftMask,		                XK_u,				      spawn,	        SHCMD("$HOME/Singularis/local/scripts/search") },
-  { MODKEY|ShiftMask,		                XK_m,				      spawn,	        SHCMD("$HOME/Singularis/local/scripts/music") },
-
-  { MODKEY,							                XK_n,							spawn,					SHCMD(TERMINAL " -e nvim") },
-  { MODKEY,							                XK_m,							spawn,					SHCMD(TERMINAL " -e neomutt") },
-  { MODKEY,							                XK_e,							spawn,					SHCMD(TERMINAL " -e ncmpcpp") },
-  { MODKEY,							                XK_c,							spawn,					SHCMD(TERMINAL " -e calcurse") },
-  { MODKEY|ShiftMask,	                  XK_k,							spawn,					SHCMD(TERMINAL " -e sudo kmon") },
-  { MODKEY|ShiftMask,		                XK_y,							spawn,					SHCMD(TERMINAL " -e sudo ytop") },
-  { MODKEY|ShiftMask,		                XK_p,							spawn,					SHCMD(TERMINAL " -e pavucontrol") },
-  { MODKEY|ShiftMask,		                XK_b,							spawn,					SHCMD(TERMINAL " -e bashtop") },
-  { MODKEY|ShiftMask,		                XK_w,							spawn,					SHCMD(TERMINAL " -e newsboat") },
-  { MODKEY|ShiftMask,		                XK_r,							spawn,					SHCMD(TERMINAL " -e htop") },
-  { MODKEY|ControlMask,		              XK_d,							spawn,					SHCMD("$HOME/Singularis/local/scripts/xmenu-script") },
-  { MODKEY|ControlMask,		              XK_s,							spawn,					SHCMD("tdrop -am -n 0 xfce4-terminal") },
-
-  { MODKEY,							                XK_z,							incrgaps,				{.i = +3 } },
-  { MODKEY,							                XK_x,							incrgaps,				{.i = -3 } },
-  { MODKEY,							                XK_b,							togglebar,			{0} },
-
-  { MODKEY|AltMask,  		                XK_t,							spawn,					SHCMD("mpc toggle") },
-  { MODKEY|AltMask,  		                XK_r,				      spawn,					SHCMD("mpc seek 0") },
-  { MODKEY|AltMask|ShiftMask,		        XK_bracketleft,		spawn,					SHCMD("mpc seek +60") },
-  { MODKEY|AltMask|ShiftMask,		        XK_bracketright,	spawn,					SHCMD("mpc seek -60") },
-  { MODKEY|AltMask,							        XK_bracketleft,		spawn,					SHCMD("mpc seek +10") },
-  { MODKEY|AltMask,							        XK_bracketright,	spawn,					SHCMD("mpc seek -10") },
-  { MODKEY|AltMask,			                XK_comma,					spawn,					SHCMD("mpc prev") },
-  { MODKEY|AltMask,			                XK_period,				spawn,					SHCMD("mpc next") },
-  { MODKEY|AltMask,	                    XK_u,				      spawn,					SHCMD("pamixer -i 5 && $HOME/Singularis/local/scripts/volume-indicator show") },
-  { MODKEY|AltMask,	                    XK_d,				      spawn,					SHCMD("pamixer -d 5 && $HOME/Singularis/local/scripts/volume-indicator show") },
-  { MODKEY|AltMask,  		                XK_v,							spawn,					SHCMD("$HOME/.config/rofi/applets/applets/mpd.sh") },
-
-  { MODKEY,							                XK_Left,					focusmon,				{.i = -1 } },
-  { MODKEY,							                XK_Right,					focusmon,				{.i = +1 } },
-  { MODKEY|ShiftMask,		                XK_Left,					tagmon,					{.i = -1 } },
-  { MODKEY|ShiftMask,		                XK_Right,					tagmon,					{.i = +1 } },
-
-  { MODKEY|AltMask,	 		                XK_c,							spawn,					SHCMD("python3.10 $HOME/Singularis/local/scripts/school/rofi-current-course.py") },
-  { MODKEY|AltMask,	 		                XK_l,							spawn,					SHCMD("python3.10 $HOME/Singularis/local/scripts/school/rofi-lessons.py") },
-  { MODKEY|AltMask,	 		                XK_g,							spawn,					SHCMD("python3.10 $HOME/Singularis/local/scripts/school/rofi-grades.py") },
-  { MODKEY|AltMask,	 		                XK_e,							spawn,					SHCMD("python3.10 $HOME/Singularis/local/scripts/school/rofi-new-lesson.py") },
-  { MODKEY|AltMask,	 		                XK_p,							spawn,					SHCMD("python3.10 $HOME/Singularis/local/scripts/school/rofi-commands.py") },
-  { MODKEY|AltMask,	 		                XK_y,							spawn,					SHCMD("$HOME/Singularis/local/scripts/school/rofi-yaml.sh") },
-  { MODKEY|AltMask,	 		                XK_n,							spawn,					SHCMD("$HOME/Singularis/local/scripts/school/rofi-nvim.sh") },
-  { MODKEY|AltMask,	 		                XK_s,							spawn,					SHCMD("$HOME/Singularis/local/scripts/school/rofi-source-code.sh") },
-  { MODKEY|AltMask,	 		                XK_w,							spawn,					SHCMD("$HOME/Singularis/local/scripts/school/rofi-web-browser.sh") },
-  { MODKEY|AltMask,	 		                XK_f,							spawn,					SHCMD("$HOME/Singularis/local/scripts/school/rofi-file-browser.sh") },
-  { MODKEY|AltMask,	 		                XK_i,							spawn,					SHCMD("$HOME/Singularis/local/scripts/school/rofi-inkscape.sh") },
-  { MODKEY|AltMask,	 		                XK_z,							spawn,					SHCMD("$HOME/Singularis/local/scripts/school/rofi-zathura.sh") },
-  { MODKEY|AltMask,	 		                XK_o,							spawn,					SHCMD("$HOME/Singularis/local/scripts/school/rofi-compile.sh") },
-
-  { MODKEY|ShiftMask,		                XK_s,	            spawn,	        SHCMD("transset-df -a --dec .1") },
-  { MODKEY|ShiftMask,		                XK_d,	            spawn,	        SHCMD("transset-df -a --inc .1") },
-  { MODKEY|ShiftMask,		                XK_f,	            spawn,	        SHCMD("transset-df -a .9") },
-
-  { MODKEY,							                XK_space,					zoom,						{0} },
-  { MODKEY|ShiftMask,		                XK_space,					togglefloating,	{0} },
-
-  { ControlMask,		                    XK_space,					spawn,	        SHCMD("dunstctl close") },
-  { ControlMask|ShiftMask,		          XK_space,					spawn,	        SHCMD("dunstctl close-all") },
-  { ControlMask,		                    XK_grave,					spawn,	        SHCMD("dunstctl history-pop") },
-};
+#include "keys.h"
 
 static Button buttons[] = {
 #ifndef __OpenBSD__
@@ -251,4 +139,3 @@ static Button buttons[] = {
   { ClkTagBar,						0,							Button5,				shiftview,			{.i = 1} },
   { ClkRootWin,						0,							Button2,				togglebar,			{0} },
 };
-
---
+++ keys.h
@@ -0,0 +1,119 @@
+static const Rule rules[] = {
+	// class      instance      title    	        tags mask     isfloating   isterminal  noswallow  monitor
+	{ "Gimp",       NULL,         NULL,             1,            0,          0,          0,          -1 },
+	{ "Gimp",       NULL,         NULL,             1 << 8,       0,          0,          0,          -1 },
+	{ TERMCLASS,    NULL,         NULL,       	    0,            0,          1,          0,           0 },
+	{ NULL,         NULL,         "Event Tester",   0,            0,          0,          0,           1 },
+	{ NULL,         "spterm",     NULL,       	    SPTAG(0),     1,          1,          0,           0 },
+	{ NULL,         "key_pdf",    NULL,       	    0,			      1,          1,          1,           0 },
+};
+
+static Key keys[] = {
+  STACKKEYS(MODKEY,                          focus)
+  STACKKEYS(MODKEY|ShiftMask,                push)
+  TAGKEYS(			XK_1,		0)
+  TAGKEYS(			XK_2,		1)
+  TAGKEYS(			XK_3,		2)
+  TAGKEYS(			XK_4,		3)
+  TAGKEYS(			XK_5,		4)
+  TAGKEYS(			XK_6,		5)
+  TAGKEYS(			XK_7,		6)
+  TAGKEYS(			XK_8,		7)
+  TAGKEYS(			XK_9,		8)
+  { MODKEY,							                XK_w,							spawn,					SHCMD(BROWSER) },
+  { MODKEY,							                XK_F1,						spawn,					SHCMD("zathura ~/Singularis/aspects/dwm/keys.pdf") },
+
+  // Open Applications
+  { MODKEY,							                XK_Return,				spawn,					{.v = termcmd } },
+  { MODKEY,                             XK_n,             spawn,          SHCMD(TERMINAL " -e 'nvim'") },
+  { MODKEY,                             XK_b,             spawn,          SHCMD(TERMINAL " -e 'newsboat'") },
+  { MODKEY,                             XK_m,             spawn,          SHCMD(TERMINAL " -e 'neomutt'") },
+  { MODKEY,                             XK_c,             spawn,          SHCMD(TERMINAL " -e 'calcurse'") },
+  { MODKEY,                             XK_c,             spawn,          SHCMD(TERMINAL " -e 'calcurse'") },
+  { MODKEY,							                XK_e,							spawn,					SHCMD(TERMINAL " -e ncmpcpp") },
+  { MODKEY|ShiftMask,	                  XK_k,							spawn,					SHCMD(TERMINAL " -e sudo kmon") },
+  { MODKEY|ShiftMask,		                XK_y,							spawn,					SHCMD(TERMINAL " -e sudo ytop") },
+  { MODKEY|ShiftMask,		                XK_p,							spawn,					SHCMD(TERMINAL " -e pavucontrol") },
+  { MODKEY|ShiftMask,		                XK_b,							spawn,					SHCMD(TERMINAL " -e bashtop") },
+  { MODKEY|ShiftMask,		                XK_g,							spawn,					SHCMD(TERMINAL " -e gotop") },
+  { MODKEY|ShiftMask,		                XK_t,							spawn,					SHCMD(TERMINAL " -e s-tui") },
+  { MODKEY|ShiftMask,		                XK_d,							spawn,					SHCMD("$HOME/Singularis/local/scripts/xmenu-script") },
+  { MODKEY|ShiftMask,		                XK_z,							spawn,					SHCMD("zathura") },
+  { MODKEY|ShiftMask,		                XK_s,							spawn,					SHCMD("tdrop -am -n 0 xfce4-terminal") },
+
+  // Rofi bindings
+  { MODKEY,                             XK_r,             spawn,          SHCMD("rofi -show drun -show-icons") },
+  { MODKEY|ShiftMask,                   XK_r,             spawn,          SHCMD("rofi -show emoji -modi emoji") },
+  { MODKEY|AltMask,                     XK_r,             spawn,          SHCMD("rofi -show window") },
+  { MODKEY,							                XK_BackSpace,			spawn,					SHCMD("$HOME/.config/rofi/applets/menu/powermenu.sh") },
+  
+  // MPC specific keybindings
+  { MODKEY|AltMask,  		                XK_t,							spawn,					SHCMD("mpc toggle") },
+  { MODKEY|AltMask,  		                XK_r,				      spawn,					SHCMD("mpc seek 0") },
+  { MODKEY|AltMask|ShiftMask,		        XK_bracketleft,		spawn,					SHCMD("mpc seek +60") },
+  { MODKEY|AltMask|ShiftMask,		        XK_bracketright,	spawn,					SHCMD("mpc seek -60") },
+  { MODKEY|AltMask,							        XK_bracketleft,		spawn,					SHCMD("mpc seek +10") },
+  { MODKEY|AltMask,							        XK_bracketright,	spawn,					SHCMD("mpc seek -10") },
+  { MODKEY|AltMask,			                XK_comma,					spawn,					SHCMD("mpc prev") },
+  { MODKEY|AltMask,			                XK_period,				spawn,					SHCMD("mpc next") },
+  { MODKEY|AltMask,	                    XK_u,				      spawn,					SHCMD("pamixer -i 5 && $HOME/Singularis/local/scripts/volume-indicator show") },
+  { MODKEY|AltMask,	                    XK_d,				      spawn,					SHCMD("pamixer -d 5 && $HOME/Singularis/local/scripts/volume-indicator show") },
+  { MODKEY|AltMask,  		                XK_v,							spawn,					SHCMD("$HOME/.config/rofi/applets/applets/mpd.sh") },
+
+  // Dunst specific keybindings
+  { ControlMask,		                    XK_space,					spawn,	        SHCMD("dunstctl close") },
+  { ControlMask|ShiftMask,		          XK_space,					spawn,	        SHCMD("dunstctl close-all") },
+  { ControlMask,		                    XK_grave,					spawn,	        SHCMD("dunstctl history-pop") },
+
+  // School specific keybindings
+  { MODKEY|AltMask,	 		                XK_c,							spawn,					SHCMD(MAIN_PATH " --change-course") },
+  { MODKEY|AltMask,	 		                XK_l,							spawn,					SHCMD(MAIN_PATH " --lessons") },
+  { MODKEY|AltMask,	 		                XK_g,							spawn,					SHCMD(MAIN_PATH " --grades") },
+  { MODKEY|AltMask,	 		                XK_e,							spawn,					SHCMD(MAIN_PATH " --new-lesson") },
+  { MODKEY|AltMask,	 		                XK_m,							spawn,					SHCMD(MAIN_PATH " --commands") },
+  { MODKEY|AltMask,	 		                XK_p,							spawn,					SHCMD(MAIN_PATH " --projects") },
+  { MODKEY|AltMask,	 		                XK_y,							spawn,					SHCMD("$HOME/Singularis/local/school/stand-alones/rofi-yaml.sh") },
+  { MODKEY|AltMask,	 		                XK_n,							spawn,					SHCMD("$HOME/Singularis/local/school/stand-alones/rofi-nvim.sh") },
+  { MODKEY|AltMask,	 		                XK_s,							spawn,					SHCMD("$HOME/Singularis/local/school/stand-alones/rofi-source-code.sh") },
+  { MODKEY|AltMask,	 		                XK_w,							spawn,					SHCMD("$HOME/Singularis/local/school/stand-alones/rofi-web-browser.sh") },
+  { MODKEY|AltMask,	 		                XK_f,							spawn,					SHCMD("$HOME/Singularis/local/school/stand-alones/rofi-file-browser.sh") },
+  { MODKEY|AltMask,	 		                XK_i,							spawn,					SHCMD("$HOME/Singularis/local/school/stand-alones/rofi-inkscape.sh") },
+  { MODKEY|AltMask,	 		                XK_z,							spawn,					SHCMD("$HOME/Singularis/local/school/stand-alones/rofi-zathura.sh") },
+  { MODKEY|AltMask,	 		                XK_o,							spawn,					SHCMD("$HOME/Singularis/local/school/stand-alones/rofi-compile.sh") },
+
+  // Changing layouts
+  { MODKEY|AltMask,							        XK_0,							setlayout,			{.v = &layouts[0]} },
+  { MODKEY|AltMask,							        XK_1,							setlayout,			{.v = &layouts[1]} },
+  { MODKEY|AltMask,							        XK_2,							setlayout,			{.v = &layouts[2]} },
+  { MODKEY|AltMask,							        XK_3,							setlayout,			{.v = &layouts[3]} },
+  { MODKEY|AltMask,							        XK_4,							setlayout,			{.v = &layouts[4]} },
+  { MODKEY|AltMask,							        XK_5,							setlayout,			{.v = &layouts[5]} },
+  { MODKEY|AltMask,							        XK_6,							setlayout,			{.v = &layouts[6]} },
+  { MODKEY|AltMask,							        XK_7,							setlayout,			{.v = &layouts[7]} },
+  { MODKEY|AltMask,							        XK_8,							setlayout,			{.v = &layouts[8]} },
+
+  // DWM specific keybindings
+  { MODKEY,							                XK_Tab,						view,						{0} },
+  { MODKEY,							                XK_o,							incnmaster,			{.i = +1 } },
+  { MODKEY|ShiftMask,		                XK_o,							incnmaster,			{.i = -1 } },
+  { MODKEY,							                XK_q,							killclient,			{0} },
+  { MODKEY|ShiftMask,                   XK_q,							spawn,					SHCMD("kill -HUP $(pidof -s dwm) && xdotool key Super_L") },
+  { MODKEY|ShiftMask|AltMask,           XK_q,							quit,						{0} },
+  { MODKEY|AltMask,                     XK_b,							togglebar,			{0} },
+  { MODKEY|ShiftMask|AltMask,           XK_b,							spawn,					SHCMD("killall -q dwmblocks; dwmblocks &") },
+  { MODKEY,							                XK_a,							togglegaps,			{0} },
+  { MODKEY,							                XK_z,							incrgaps,				{.i = +3 } },
+  { MODKEY,							                XK_x,							incrgaps,				{.i = -3 } },
+  { MODKEY,							                XK_s,							togglesticky,		{0} },
+  { MODKEY,							                XK_f,							togglefullscr,	{0} },
+  { MODKEY,							                XK_h,							setmfact,				{.f = -0.05} },
+  { MODKEY,							                XK_l,							setmfact,      	{.f = +0.05} },
+  { MODKEY|ShiftMask,		                XK_semicolon,			shifttag,				{ .i = 1 } },
+  { MODKEY,							                XK_Left,					focusmon,				{.i = -1 } },
+  { MODKEY,							                XK_Right,					focusmon,				{.i = +1 } },
+  { MODKEY|ShiftMask,		                XK_Left,					tagmon,					{.i = -1 } },
+  { MODKEY|ShiftMask,		                XK_Right,					tagmon,					{.i = +1 } },
+  { MODKEY,							                XK_space,					zoom,						{0} },
+  { MODKEY|ShiftMask,		                XK_space,					togglefloating,	{0} },
+};
+
